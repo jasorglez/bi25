f6a53382683b107f200acde1574dcf0268420c4f
Jose Angel Soriano <jsorglez@gmail.com>
2024-07-09 14:00:55 -0600

    Commit inicial para el Super proyecto Bi julio 2024

---
+++ login/pages/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ComploginComponent } from '../../components/complogin/complogin.component';
+
+
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [ComploginComponent],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+}
